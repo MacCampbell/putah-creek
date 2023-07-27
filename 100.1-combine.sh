fe0b00174a376cc559f5009279fc649398380cea
maccampbell <campbellmatthewa@gmail.com>
2023-07-27 13:58:32 -0800

    Combining sequence data

---
+++ 100.1-combine.sh
@@ -0,0 +1,39 @@
+cat data/raw/Project_ASEF_BMAG077_L1/PUT014RAD_S7_L005_R1_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT014RAD_S7_L006_R1_001.fastq.gz > data/raw/PUT014RAD-R1.fastq.gz
+cat data/raw/Project_ASEF_BMAG077_L1/PUT015RAD_S8_L005_R1_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT015RAD_S8_L006_R1_001.fastq.gz > data/raw/PUT015RAD-R1.fastq.gz
+cat data/raw/Project_ASEF_BMAG077_L1/PUT016RAD_S9_L005_R1_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT016RAD_S9_L006_R1_001.fastq.gz > data/raw/PUT016RAD-R1.fastq.gz 
+cat data/raw/Project_ASEF_BMAG077_L1/PUT017RAD_S1_L005_R1_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT017RAD_S1_L006_R1_001.fastq.gz > data/raw/PUT017RAD-R1.fastq.gz
+cat data/raw/Project_ASEF_BMAG077_L1/PUT018RAD_S2_L005_R1_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT018RAD_S2_L006_R1_001.fastq.gz > data/raw/PUT018RAD-R1.fastq.gz
+cat data/raw/Project_ASEF_BMAG077_L1/PUT019RAD_S3_L005_R1_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT019RAD_S3_L006_R1_001.fastq.gz > data/raw/PUT019RAD-R1.fastq.gz
+cat data/raw/Project_ASEF_BMAG077_L1/PUT020RAD_S4_L005_R1_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT020RAD_S4_L006_R1_001.fastq.gz > data/raw/PUT020RAD-R1.fastq.gz
+cat data/raw/Project_ASEF_BMAG077_L1/PUT021RAD_S5_L005_R1_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT021RAD_S5_L006_R1_001.fastq.gz > data/raw/PUT021RAD-R1.fastq.gz
+cat data/raw/Project_ASEF_BMAG077_L1/PUT022RAD_S6_L005_R1_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT022RAD_S6_L006_R1_001.fastq.gz > data/raw/PUT022RAD-R1.fastq.gz
+
+cat data/raw/Project_ASEF_BMAG077_L1/PUT014RAD_S7_L005_R2_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT014RAD_S7_L006_R2_001.fastq.gz > data/raw/PUT014RAD-R2.fastq.gz
+cat data/raw/Project_ASEF_BMAG077_L1/PUT015RAD_S8_L005_R2_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT015RAD_S8_L006_R2_001.fastq.gz > data/raw/PUT015RAD-R2.fastq.gz
+cat data/raw/Project_ASEF_BMAG077_L1/PUT016RAD_S9_L005_R2_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT016RAD_S9_L006_R2_001.fastq.gz > data/raw/PUT016RAD-R2.fastq.gz 
+cat data/raw/Project_ASEF_BMAG077_L1/PUT017RAD_S1_L005_R2_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT017RAD_S1_L006_R2_001.fastq.gz > data/raw/PUT017RAD-R2.fastq.gz
+cat data/raw/Project_ASEF_BMAG077_L1/PUT018RAD_S2_L005_R2_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT018RAD_S2_L006_R2_001.fastq.gz > data/raw/PUT018RAD-R2.fastq.gz
+cat data/raw/Project_ASEF_BMAG077_L1/PUT019RAD_S3_L005_R2_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT019RAD_S3_L006_R2_001.fastq.gz > data/raw/PUT019RAD-R2.fastq.gz
+cat data/raw/Project_ASEF_BMAG077_L1/PUT020RAD_S4_L005_R2_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT020RAD_S4_L006_R2_001.fastq.gz > data/raw/PUT020RAD-R2.fastq.gz
+cat data/raw/Project_ASEF_BMAG077_L1/PUT021RAD_S5_L005_R2_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT021RAD_S5_L006_R2_001.fastq.gz > data/raw/PUT021RAD-R2.fastq.gz
+cat data/raw/Project_ASEF_BMAG077_L1/PUT022RAD_S6_L005_R2_001.fastq.gz data/raw/Project_ASEF_BMAG077_L2/PUT022RAD_S6_L006_R2_001.fastq.gz > data/raw/PUT022RAD-R2.fastq.gz
+
+gunzip data/raw/PUT014RAD-R1.fastq.gz
+gunzip data/raw/PUT015RAD-R1.fastq.gz
+gunzip data/raw/PUT016RAD-R1.fastq.gz
+gunzip data/raw/PUT017RAD-R1.fastq.gz
+gunzip data/raw/PUT018RAD-R1.fastq.gz
+gunzip data/raw/PUT019RAD-R1.fastq.gz
+gunzip data/raw/PUT020RAD-R1.fastq.gz
+gunzip data/raw/PUT021RAD-R1.fastq.gz
+gunzip data/raw/PUT022RAD-R1.fastq.gz
+
+gunzip data/raw/PUT014RAD-R2.fastq.gz
+gunzip data/raw/PUT015RAD-R2.fastq.gz
+gunzip data/raw/PUT016RAD-R2.fastq.gz
+gunzip data/raw/PUT017RAD-R2.fastq.gz
+gunzip data/raw/PUT018RAD-R2.fastq.gz
+gunzip data/raw/PUT019RAD-R2.fastq.gz
+gunzip data/raw/PUT020RAD-R2.fastq.gz
+gunzip data/raw/PUT021RAD-R2.fastq.gz
+gunzip data/raw/PUT022RAD-R2.fastq.gz
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ For examining parentage of Putah Creek Chinook
 
 Putah Creek is a Sacramento River tributary with returning Chinook salmon. It has been known to have a fall run population (Yoshiyama et al. 1998). Otolith microchemistry indicates salmon spawning in Putah Creek now are from a variety of sources, ~88% appear to be from Central Valley hatcheries (https://afspubs.onlinelibrary.wiley.com/doi/full/10.1002/fsh.10516).   The ability of juveniles to survive and return to spawn in Putah Creek, and the creation of a self-sustaining population is unknown.   We have a set of Chinook salmon from 2016-2021 and the goal is to identify family-level relationships to see if any of these fish produced offspring.  
 
-Most Chinook salmon in the stream are probably 2-3 years old (Freshwater annuli=0, saltwater annuli=2-3), with possilbe 4 year old fish.
+Most Chinook salmon in the stream are probably 2-3 years old (Freshwater annuli=0, saltwater annuli=2-3), with a few 4 year old fish.
