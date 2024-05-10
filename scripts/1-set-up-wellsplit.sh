a2a78bd4e8a0f6c419a4f2b330189ab55c26cbf6
maccampbell <campbellmatthewa@gmail.com>
2024-05-10 16:28:36 +1000

    Trying 2.5 counts

--- scripts/1-set-up-wellsplit.sh
+++ scripts/1-set-up-wellsplit.sh
@@ -3,5 +3,5 @@
 
 ls *R1*.fastq > listR1
 ls *R2*.fastq > listR2
-ls *-R1* | sed 's/-R1[^=&]*//g' | paste listR1 listR2 - > file-list.txt
+ls *_R1* | sed 's/_R1[^=&]*//g' | paste listR1 listR2 - > file-list.txt
 
